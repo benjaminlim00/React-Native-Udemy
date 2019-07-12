01285144c986de16a0cf1fb94cb0ae401f493da7
benjaminlim00 <benjaminlim1996@hotmail.sg>
2019-07-12 09:07:35 +0800

    updated

---
+++ auth/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { Header } from "./components/common";
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header headerTest="Authentication" />
+        <Text>App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ auth/src/components/common/index.js
@@ -0,0 +1,10 @@
+//import magic. When we import from a folder without
+//specifying file, it will look for index.js
+
+export * from "./Button";
+export * from "./Card";
+export * from "./CardSection";
+export * from "./Header";
+
+//note: have to remove the default key word
+//from export default in the individual files
