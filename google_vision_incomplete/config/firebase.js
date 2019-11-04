f044a83a754ab07f564482bb7268c07a91bbdf4d
benjamin <benjaminlim1996@hotmail.sg>
2019-11-05 00:04:49 +0900

    google vision imcomplete

---
+++ google_vision_incomplete/config/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBuH3sSCWYUXfVtM8t4qHYXr8-nI-uVPC4',
+  authDomain: 'react-native-tutorial-7798d.firebaseapp.com',
+  databaseURL: 'https://react-native-tutorial-7798d.firebaseio.com',
+  projectId: 'react-native-tutorial-7798d',
+  storageBucket: 'react-native-tutorial-7798d.appspot.com',
+  messagingSenderId: '595404914402',
+  appId: '1:595404914402:web:79a04a23b43e826ce2a8bb',
+};
+
+export default firebase;
